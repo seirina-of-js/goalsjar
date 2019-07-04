062810e2ae36a725f7f02ca575732a0ce88c67e8
galina-prokofeva <g.k.prokofeva@gmail.com>
2019-07-05 01:05:44 +0300

    webpack config

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className={classes.App}>
+                Goalsjar
+            </div>
+    )
+    }
+};
+
+export default App;
